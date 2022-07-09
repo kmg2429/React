201d600c3a13b26f6e2744f7fbcef66dd46d2bc0
MinGyu <51067546+kmg2429@users.noreply.github.com>
2022-07-09 23:06:24 +0900

    Add files via upload

---
+++ dicegame/src/index.js
@@ -0,0 +1,13 @@
+
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <h1 id="title">가위바위보</h1>
+<button class="hand">가위</button>
+<button class="hand">바위</button>
+<button class="hand">보</button>
+  </>
+);
