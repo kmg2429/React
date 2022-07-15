416a68fdfb75ff3607cc440e38ffe6a2852a34ec
MinGyu <51067546+kmg2429@users.noreply.github.com>
2022-07-15 22:58:05 +0900

    Add files via upload

--- rock-scissor-paper/src/HandIcon.js
+++ rock-scissor-paper/src/HandIcon.js
@@ -3,12 +3,12 @@ import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 
 const IMAGES = {
-  rock:rockImg,
-  scissor:scissorImg,
-  paper:paperImg,
-}
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
 
-function HandIcon({value}) {
+function HandIcon({ value }) {
   const src = IMAGES[value];
   return <img src={src} alt={value} />;
 }
