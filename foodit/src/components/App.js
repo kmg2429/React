46626408bbf78b672582464b561a8959368e2ea3
MinGyu <51067546+kmg2429@users.noreply.github.com>
2022-07-26 22:41:57 +0900

    Add files via upload

---
+++ foodit/src/components/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import FoodList from './FoodList';
+import items from '../mock.json';
+
+function App() {
+  const [order, setOrder] = useState('createdAt');
+
+  const handleNewestClick = () => setOrder('createdAt');
+
+  const handleCalorieClick = () => setOrder('calorie');
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  return (
+    <div>
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} />
+    </div>
+  );
+}
+
+export default App;
